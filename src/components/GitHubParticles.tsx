a41d05c9e3681d06260a9d960b83c12d79d65c32
Katrina Finney <katrinafinney@gmail.com>
2025-02-21 11:03:13 -0500

    deployment

--- src/components/GitHubParticles.tsx
+++ src/components/GitHubParticles.tsx
@@ -44,8 +44,8 @@ export default function GitHubParticles() {
             key={repo.id}
             position={[Math.random() * 6 - 3, Math.random() * 6 - 3, Math.random() * 6 - 3]}
             onClick={() => router.push(repo.html_url)}
-            onPointerOver={(_e) => (document.body.style.cursor = "pointer")}
-            onPointerOut={(_e) => (document.body.style.cursor = "default")}
+            onPointerOver={() => (document.body.style.cursor = "pointer")}
+            onPointerOut={() => (document.body.style.cursor = "default")}
           >
             <Sphere args={[0.3, 32, 32]}>
               <MeshDistortMaterial
