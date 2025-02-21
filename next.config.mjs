69dcc3095f95ac6d43ed6de84793028efeb910de
Katrina Finney <katrinafinney@gmail.com>
2025-02-21 18:44:56 -0500

    updated favicon

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true, // Ensures public images load correctly
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig
+
+
+
